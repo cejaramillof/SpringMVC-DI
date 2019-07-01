617f7c4a20e1964eb65079c9cc01d891deb57ee4
Carlos Jaramillo <cejarami@bancolombia.com.co>
2019-07-01 13:23:52 -0500

    map routes and send data controller to view

--- src/main/java/com/example/springbootweb/controllers/IndexController.java
+++ src/main/java/com/example/springbootweb/controllers/IndexController.java
@@ -1,17 +1,37 @@
 package com.example.springbootweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
-    //@RequestMapping(value = "/index", method = RequestMethod.GET)
-    //@GetMapping("/index")
+    /**
+     * @RequestMapping(value = "/index", method = RequestMethod.GET)
+     *
+     * Model is an interface with some abstract methods (it goes throught argument aa implementation of this interface)
+     * @param model
+     * @return
+     */
     @GetMapping({"/index", "/"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("title", "Spring Index");
         return "index";
+    }
+
+    /**
+     * Implement ModelMap
+     * @param mv
+     * @return
+     */
+    @GetMapping("/home")
+    public ModelAndView home(ModelAndView mv) {
+        mv.setViewName("index");
+        mv.addObject("title", "Spring Home");
+        return mv;
     }
 }
