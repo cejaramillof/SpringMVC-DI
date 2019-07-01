ade243109ff263b0199fb1532baa3caf249429e4
Carlos Jaramillo <cejarami@bancolombia.com.co>
2019-07-01 14:32:42 -0500

    basic mvc

--- src/main/java/com/example/springbootweb/controllers/IndexController.java
+++ src/main/java/com/example/springbootweb/controllers/IndexController.java
@@ -1,10 +1,10 @@
 package com.example.springbootweb.controllers;
 
+import com.example.springbootweb.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -34,5 +34,16 @@ public class IndexController {
         mv.setViewName("index");
         mv.addObject("title", "Spring Home ModelAndView");
         return mv;
+    }
+
+    @GetMapping("/profile")
+    public String profile(Model model) {
+        User user = new User();
+        user.setName("Carlos");
+        user.setLastName("Jaramillo");
+
+        model.addAttribute("user", user);
+        model.addAttribute("title", "User Profile");
+        return "profile";
     }
 }
