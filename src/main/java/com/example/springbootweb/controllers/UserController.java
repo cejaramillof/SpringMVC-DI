9975806384c48051652ece5d6bce26a287ae1e94
Carlos Jaramillo <cejarami@bancolombia.com.co>
2019-07-01 20:15:58 -0500

    send data to all controllers

--- src/main/java/com/example/springbootweb/controllers/UserController.java
+++ src/main/java/com/example/springbootweb/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.example.springbootweb.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -15,13 +16,6 @@ public class UserController {
 
     @GetMapping({"/", ""})
     public String index(Model model) {
-        List<User> users = Arrays.asList(
-                new User("Carlos", "Jaramillo", "c@xd.com"),
-                new User("Eduardo", "Franco", "e@xd.com"),
-                new User("Tyrone", "Jaramillo", "t@xd.com")
-        );
-
-        model.addAttribute("users", users);
         model.addAttribute("title", "User list");
 
         return "user/index";
@@ -29,7 +23,6 @@ public class UserController {
 
     @GetMapping("/show")
     public String show(Model model) {
-
         User user = new User();
 
         user.setName("Carlos");
@@ -39,5 +32,18 @@ public class UserController {
         model.addAttribute("title", "User Profile");
 
         return "user/show";
+    }
+
+    /**
+     * @return List of all users to all Methods of this controller
+     */
+    @ModelAttribute("users")
+    public List<User> usersList() {
+        List<User> users = Arrays.asList(
+                new User("Carlos", "Jaramillo", "c@xd.com"),
+                new User("Eduardo", "Franco", "e@xd.com"),
+                new User("Tyrone", "Jaramillo", "t@xd.com")
+        );
+        return users;
     }
 }
