c4a06defaed5def49fbe73793b4f0e7ea24cf982
Carlos Jaramillo <cejarami@bancolombia.com.co>
2019-07-04 13:19:13 -0500

    compile and run

--- src/main/java/com/example/springbootweb/SpringBootWebApplication.java
+++ src/main/java/com/example/springbootweb/SpringBootWebApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootWebApplication {
 
+    // to compile app use mvn package and he return jar in target and run java-jar ./target/appname.jar
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
